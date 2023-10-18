0e8f7a99b36a2404bb684f12f56cb6352ff33e5c
bouncingbumble <jordboudreau@gmail.com>
2023-10-18 16:46:40 -0700

    add handle for message sent

--- api/text.js
+++ api/text.js
@@ -3,52 +3,55 @@ const { v4: uuidv4 } = require('uuid')
 var telnyx = require('telnyx')(process.env.TELYNX_API_KEY)
 
 exports.incomingText = async (req, res, next) => {
-    const from = req.body.data.payload.from.phone_number
-    const messageBody = req.body.data.payload.text
+    if (req.body.data.event_type === 'message.sent') {
+        const from = req.body.data.payload.from.phone_number
+        const messageBody = req.body.data.payload.text
 
-    const user = await db.UserSettings.findOne({ phoneNumber: from })
+        const user = await db.UserSettings.findOne({ phoneNumber: from })
 
-    if (user === null) {
-        // this.sendText(
-        //     from,
-        //     'We were unable to find an account with that number. Make sure you have added your number correctly in your user profile.'
-        // )
-    } else {
-        try {
-            await db.Task.create({
-                name: messageBody,
-                user: user.user,
-                isInbox: true,
-                key: uuidv4(),
-            })
-
-            io.emit('new task', {
-                userId: user.id,
-            })
-
+        if (user === null) {
             this.sendText(
-                user.phoneNumber,
-                'Task Added! Go to https://app.llamalist.com/tasks/inbox/All%20Labels to view.'
+                from,
+                'We were unable to find an account with that number. Make sure you have added your number correctly in your user profile.'
             )
-        } catch (err) {
-            console.log(err)
+        } else {
+            try {
+                await db.Task.create({
+                    name: messageBody,
+                    user: user.user,
+                    isInbox: true,
+                    key: uuidv4(),
+                })
+
+                io.emit('new task', {
+                    userId: user.id,
+                })
+
+                this.sendText(
+                    user.phoneNumber,
+                    'Task Added! Go to https://app.llamalist.com/tasks/inbox/All%20Labels to view.'
+                )
+            } catch (err) {
+                console.log(err)
+            }
         }
     }
-
     res.sendStatus(200)
 }
 
-exports.sendText = async (to, text, media_urls) => {
-    // telnyx.messages.create(
-    //     {
-    //         from: process.env.TELYNX_NUMBER, // Your Telnyx number
-    //         to,
-    //         text,
-    //         media_urls,
-    //     },
-    //     function (err, response) {
-    //         // asynchronously called
-    //         console.log(response)
-    //     }
-    // )
+exports.sendText = (to, text, media_urls) => {
+    if (to !== process.env.TELYNX_NUMBER) {
+        telnyx.messages.create(
+            {
+                from: process.env.TELYNX_NUMBER, // Your Telnyx number
+                to,
+                text,
+                media_urls,
+            },
+            function (err, response) {
+                // asynchronously called
+                console.log(response)
+            }
+        )
+    }
 }
